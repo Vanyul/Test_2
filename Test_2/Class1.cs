9c324375f3b35f0325f319bfd32cf552f3384f52
Vanyul <164251530+Vanyul@users.noreply.github.com>
2025-07-29 13:57:27 +0300

    Test_2

---
+++ Test_2/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace Test_2
+{
+    using System;
+    using System.Threading;
+
+    public static class Server
+    {
+        private static int count = 0;
+
+        // Блокировка для управления параллельным доступом
+        private static ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+
+        // Метод чтения count (много читателей одновременно)
+        public static int GetCount()
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+
+        // Метод добавления значения к count (писатели — по одному)
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+    }
+
+}
